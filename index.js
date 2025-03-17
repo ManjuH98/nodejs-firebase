6a4cb65aeb9a93d451317c7245898b8e64fa6df2
ManjuH98 <HManjunath@yathisolutions.com>
2025-03-17 16:18:48 +0530

    code clean up and if schedule is available then show or else immetiate.

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-// console.log("Sample Application")
 const admin = require("firebase-admin");
 const fs = require("fs");
 const { notificationData } = require("./notificationData")
 
 // Load your Firebase service account key
-// const serviceAccount = require("./path-to-your-service-account.json");
 const serviceAccount = require("./service-account.json");//Add this service File
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
-// Load JSON data
-// const notificationData = [
-//   {
-//     // "Device Id": "device_token_1",
-//     "Device Id": "e3WhMfqZQ8iPq7R7tZuFfm:APA91bGh3Tcdas77woZViekgwmT4WEb11CBHUSeVVfJ0Jdb3OCx6BQFiU_ycabHULChaGtGvojBtsI5VL2TGwdvD1pI834JeTS4eZ1BAqD4oYOCjcWWd6go",
-//     "Title": "Notification Title 1",
-//     "Body": "Notification Body 1"
-//   },
-//   {
-//     "Device Id": "cLhXse8IR8WvhqWNKZOs3x:APA91bEVz853Wdeu-5BP_ZF_eSWPI_X2XEFKXeplY9k-C9OSjH-9hKjqym56vtoJZUvHYpNhWOwjStn8z6ixUaLesTCqovULIEBywwEYNTR1EuedrtK4Bio",
-//     "Title": "Vinay Babu",
-//     "Body": "Body"
-//   }
-// ];
 
 // Function to send notifications
 const sendNotification = async (data) => {
@@ -34,7 +17,7 @@ const sendNotification = async (data) => {
     notification: {
       title: data["Title"],
       body: data["Body"],
-      image: data["Image"]
+      image: data["Image"] || null
     }
   };
 
@@ -48,20 +31,22 @@ const sendNotification = async (data) => {
 
 const scheduleNotification = (notification) => {
   const scheduledTime = new Date(notification["Scheduled Time"]);
-  const delay = scheduledTime - new Date();
 
-  if (delay > 0) {
-    setTimeout(() => sendNotification(notification), delay);
-    console.log("Sending message");
+  if (!scheduledTime) {
+    sendNotification(notification);
   } else {
-    console.error("Scheduled time is in the past.");
+    const scheduledDate = new Date(scheduledTime);
+    const delay = scheduledTime - new Date();
+
+    if (delay > 0) {
+      setTimeout(() => sendNotification(notification), delay);
+      console.log("Notification scheduled for:", scheduledTime);
+    } else {
+      console.error("Scheduled time is in the past. Sending immediately.");
+      sendNotification(notification);
+    }
   }
 };
-
-// Send notifications for each entry
-// notificationData.forEach((notification) => {
-//   sendNotification(notification);
-// });
 
 notificationData.forEach((notification) => {
   scheduleNotification(notification);
--- notificationData.js
+++ notificationData.js
@@ -1,33 +1,19 @@
-// const notificationData = [
-//     {
-//         "Device Id": "e3WhMfqZQ8iPq7R7tZuFfm:APA91bGh3Tcdas77woZViekgwmT4WEb11CBHUSeVVfJ0Jdb3OCx6BQFiU_ycabHULChaGtGvojBtsI5VL2TGwdvD1pI834JeTS4eZ1BAqD4oYOCjcWWd6go",
-//         "Title": "Notification Title 1",
-//         "Body": "Notification Body 1"
-//     },
-//     {
-//         "Device Id": "cLhXse8IR8WvhqWNKZOs3x:APA91bEVz853Wdeu-5BP_ZF_eSWPI_X2XEFKXeplY9k-C9OSjH-9hKjqym56vtoJZUvHYpNhWOwjStn8z6ixUaLesTCqovULIEBywwEYNTR1EuedrtK4Bio",
-//         "Title": "Vinay Babu",
-//         "Body": "Body"
-//     }
-// ];
 
-// module.exports = { notificationData };
 const notificationData = [
-    {
-      "Device Id": "e3WhMfqZQ8iPq7R7tZuFfm:APA91bGh3Tcdas77woZViekgwmT4WEb11CBHUSeVVfJ0Jdb3OCx6BQFiU_ycabHULChaGtGvojBtsI5VL2TGwdvD1pI834JeTS4eZ1BAqD4oYOCjcWWd6go",
-      "Title": "Notification Title 1",
-      "Body": "Notification Body 1",
-      "Scheduled Time": "2025-03-17T14:32:00+05:30", // Use ISO format,
-      "Image":"https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg"
+    {//Manju
+        "Device Id": "e3WhMfqZQ8iPq7R7tZuFfm:APA91bGh3Tcdas77woZViekgwmT4WEb11CBHUSeVVfJ0Jdb3OCx6BQFiU_ycabHULChaGtGvojBtsI5VL2TGwdvD1pI834JeTS4eZ1BAqD4oYOCjcWWd6go",
+        "Title": "Notification Title 1",
+        "Body": "Notification Body 1",
+        // "Scheduled Time": "2025-03-17T14:32:00+05:30",
+        "Image": "https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg"
     },
-    {
-      "Device Id": "cLhXse8IR8WvhqWNKZOs3x:APA91bEVz853Wdeu-5BP_ZF_eSWPI_X2XEFKXeplY9k-C9OSjH-9hKjqym56vtoJZUvHYpNhWOwjStn8z6ixUaLesTCqovULIEBywwEYNTR1EuedrtK4Bio",
-      "Title": "Vinay Babu",
-      "Body": "Body",
-      "Scheduled Time": "2025-03-17T14:32:30+05:30",
-      "Image":"https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg"
+    {//Vinay
+        "Device Id": "cLhXse8IR8WvhqWNKZOs3x:APA91bEVz853Wdeu-5BP_ZF_eSWPI_X2XEFKXeplY9k-C9OSjH-9hKjqym56vtoJZUvHYpNhWOwjStn8z6ixUaLesTCqovULIEBywwEYNTR1EuedrtK4Bio",
+        "Title": "Vinay Babu",
+        "Body": "Body",
+        // "Scheduled Time": "2025-03-17T15:04:00+05:30",
+        "Image": "https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg"
     }
-  ];
-  
-  module.exports = { notificationData };
-  
+];
+
+module.exports = { notificationData };
