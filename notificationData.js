6085af3454898f98be2fc0e6f81b722a9c82dbfd
ManjuH98 <HManjunath@yathisolutions.com>
2025-03-25 10:23:44 +0530

    Data changed in notificationData

--- notificationData.js
+++ notificationData.js
@@ -2,19 +2,19 @@
 const notificationData = [
     {//Manju
         "Device Id": "fs78SWruSx6E8LLBV7OXEJ:APA91bEqb_amH8pwKYhlP1xRMLmrttusEIGAZ1mQdLcsg7GWrb0_-Niu8TcC331JY4lQTlxHtYWQQZg7Xq1huRhjroaKKmm3GuquaVeNDd7BE7ETY1s-a1s",
-        "Title": "Notification Title 1",
-        "Body": "Notification Body 1",
+        "Title": "Gentle Remainder",
+        "Body": "You haven't continued learning for a few days. Please resume your progress! 😊",
         // "Scheduled Time": "2025-03-17T14:32:00+05:30",
-        "Image": "https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg",
-        "Link": "https://central.toasttab.com/s/article/Automated-Nightly-Data-Export-1492723819691"
+        "Image": "https://blog.yatradham.org/wp-content/uploads/2022/03/Statue-of-Equality-Ticket-Price-Timings-and-Online-Booking.jpg",
+        "Link": "https://www.ithihasa.chinnajeeyar.org/#/home"
     },
     {//Vinay
         "Device Id": "cLhXse8IR8WvhqWNKZOs3x:APA91bEVz853Wdeu-5BP_ZF_eSWPI_X2XEFKXeplY9k-C9OSjH-9hKjqym56vtoJZUvHYpNhWOwjStn8z6ixUaLesTCqovULIEBywwEYNTR1EuedrtK4Bio",
-        "Title": "Vinay Babu",
-        "Body": "Body",
+        "Title": "Gentle Remainder",
+        "Body": "You haven't continued learning for a few days. Please resume your progress! 😊",
         // "Scheduled Time": "2025-03-17T15:04:00+05:30",
-        "Image": "https://as2.ftcdn.net/jpg/07/04/28/55/1000_F_704285528_KC3hew86Jb013uaw2eMCkHzRS8hquCTm.jpg",
-        "Link": "https://central.toasttab.com/s/article/Automated-Nightly-Data-Export-1492723819691"
+        "Image": "https://blog.yatradham.org/wp-content/uploads/2022/03/Statue-of-Equality-Ticket-Price-Timings-and-Online-Booking.jpg",
+        "Link": "https://www.ithihasa.chinnajeeyar.org/#/home"
     }
 ];
 
